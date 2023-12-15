9b041c8ab7d4c15769abab75e4bfa59fcc3ddbb7
Roman <roman.spam2004@yaandex.ru>
2023-12-15 16:59:53 +0300

    Initial Commit

---
+++ hwPereponov_till17/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hwPereponov_till17
+//
+//  Created by Роман Перепонов on 15.12.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
